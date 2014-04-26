57fc116844d1a2ac5a80694a2d1e39c48332a283
Anthony Foster <git@af.id.au>
2014-04-26 14:40:33 +1000

    readme

--- objects/world.js
+++ objects/world.js
@@ -1,7 +1,7 @@
 module.exports = World;
 
 var fs = require('fs');
-var shaderSource = fs.readFileSync(__dirname + '/../.gitignore', 'utf8');
+var shaderSource = fs.readFileSync(__dirname + '/../README.md', 'utf8');
 console.log('example string: ', shaderSource);
 
 function World() {
