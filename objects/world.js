ce883749ec88e6c7e69c8f3a261fc5b20e4f9810
James McCormick <dajimmyjmac@hotmail.com>
2014-04-26 19:50:54 +1000

    terrain generation

--- objects/world.js
+++ objects/world.js
@@ -1,36 +1,59 @@
 module.exports = World;
 
+var SAND = 1;
+var DIRT = 2;
+var CLAY = 3;
+var ROCK = 4;
+var GOLD = 5;
+var DIAMOND = 6;
+var EMPTY = 7;
+var size = 10;
+var NUMDIAMONDS = 2;
+var NUMSPACE = 4;
+
 function World() {
 	this.blocks = [],
 
 	this.createWorld = function(){
 		this.blocks = [];
-		for(var i = 0; i < 10; i++){
+		for(var i = 0; i < size; i++){
 			this.blocks[i] = [];
 		}
+		if(NUMSPACE >= size / 2)
+			NUMSPACE = Math.floor(size / 2);
+		NUMDIAMONDS = Math.floor(size / 10) + 1;
+		this.addDiamonds();
+		this.addGold();
+		this.addRocks();
 
-		var cube;
-		for(var i = 0; i < 10; i++){
-			for(var j = 0; j < 10; j++){
-				var cube = this.makeBlock();
-				cube.position.x = i;
-				cube.position.y = j;
-				scene.add(cube);
-				this.blocks[i][j] = cube;
+		for(var i = 0; i < size; i++) {
+			for(var j = 0; j < size; j++) {
+				if(this.blocks[i][j] == undefined)
+					var r = Math.random();
+					if(r < 0.5)
+						this.makeBlock(DIRT,i,j);
+					if(r < 0.8)
+						this.makeBlock(CLAY,i,j);
+					else
+						this.makeBlock(SAND,i,j);
 			}
 		}
 	},
 
-	this.makeBlock = function(){
-		var block = this.chooseBlock();
+	this.makeBlock = function(type,i,j){
+		var block = this.chooseBlock(type);
 
 		var geometry = new THREE.CubeGeometry(0.8 + Math.random() * 0.1,0.8 + Math.random () * 0.1,0.95);
 		var material = new THREE.MeshLambertMaterial({color: block.color});
 		cube = new THREE.Mesh(geometry, material);
 		cube.rotation.set((Math.random() - 0.5) * 0.9, (Math.random()-0.5) * 0.9, 0.0);
 		// scene.add(cube);
-		cube.name = block.name;
 		block.strength = 2;
+		cube.name = block.name;
+		cube.position.x = i;
+		cube.position.y = j;
+		scene.add(cube);
+		this.blocks[i][j] = cube;
 
 		return cube;
 	},
@@ -58,35 +81,38 @@ function World() {
 		return cube;
 	};
 
-	this.chooseBlock = function(){
+	/*this.chooseBlock = function(){
 		var i = Math.floor(Math.random()*6);
 		switch(i){
-			case 0:
+			case 0:*/
+	this.chooseBlock = function(type){
+		switch(type){
+			case SAND:
 				return {
 					name: 'sand',
-					color: 0xFFFF00
+					color: 0xFF0000 //0xFFFF00
 				}
-			case 1:
+			case DIRT:
 				return {
 					name: 'dirt',
 					color: 0xFFAA00
 				}
-			case 2:
+			case CLAY:
 				return {
 					name: 'clay',
 					color: 0xFF5500
 				}
-			case 3:
+			case ROCK:
 				return {
 					name: 'rock',
 					color: 0xAAAAAA
 				}
-			case 4:
+			case GOLD:
 				return {
 					name: 'gold',
 					color: 0xFFFF00
 				}
-			case 5:
+			case DIAMOND:
 				return {
 					name: 'diamond',
 					color: 0x0000FF
@@ -98,4 +124,87 @@ function World() {
 				}
 		}
 	}
+
+	this.addDiamonds = function() {
+		var fail = false;
+		for(var i = 0; i < NUMDIAMONDS; ++ i) {
+			var x = Math.floor(Math.random()*size);
+			var y = Math.floor(Math.random()*size);
+			fail = false;
+			while(y < 4)
+				y = Math.floor(Math.random()*size);
+
+			if(this.blocks[x][y] == undefined) {
+				for(var a = Math.max(x - NUMSPACE,0); a < Math.min(x + NUMSPACE,size); ++ a) {
+					for(var b = Math.max(y - NUMSPACE,0); b < Math.min(y + NUMSPACE,size); ++ b) {
+						if((a == 0 && b == 0) || (this.blocks[a][b] != undefined && this.blocks[a][b].name == 'diamond'))
+							fail = true;
+					}
+				}
+				
+			} else 
+				fail = true;
+
+			if(fail == true)
+				-- i;
+			else
+				this.makeBlock(DIAMOND,x,y);
+		}
+	}
+
+	this.addGold = function() {
+		var numGold = Math.floor(((size * size) + 2) * 0.04);
+		var fail = false;
+		for(var i = 0; i < numGold; ++ i) {
+			var x = Math.floor(Math.random()*size);
+			var y = Math.floor(Math.random()*size);
+			fail = false;
+			while(y < 3)
+				y = Math.floor(Math.random()*size);
+			
+			if(this.blocks[x][y] == undefined) {
+				for(var a = Math.max(x - (NUMSPACE/2),0); a < Math.min(x + (NUMSPACE/2),size); ++ a) {
+					for(var b = Math.max(y - (NUMSPACE/2),0); b < Math.min(y + (NUMSPACE/2),size); ++ b) {
+						if((a == 0 && b == 0) || (this.blocks[a][b] != undefined && (this.blocks[a][b].name == 'diamond' || this.blocks[a][b].name == 'gold')))
+							fail = true;
+					}
+				}
+				
+			} else 
+				fail = true;
+
+			if(fail == true)
+				-- i;
+			else
+				this.makeBlock(GOLD,x,y);
+		}
+	}
+
+	this.addRocks = function() {
+		var numRock = (size * size) * 0.05;
+		if(size > 2) numRock += 2;
+		
+		for(var i = 0; i < numRock; ++ i) {
+			var x = Math.floor(Math.random()*size);
+			var y = Math.floor(Math.random()*size);
+			if(this.blocks[x][y] == undefined) {
+				this.makeBlock(ROCK,x,y);
+				var r = Math.random();
+				if(r < 0.5) {
+					if(x != size - 1 && this.blocks[x+1][y] == undefined) this.makeBlock(ROCK,x+1,y);
+					else if(x != 0 && this.blocks[x-1][y] == undefined) this.makeBlock(ROCK,x-1,y);
+					else if(y != size - 1 && this.blocks[x][y+1] == undefined) this.makeBlock(ROCK,x,y+1);
+					else if(y != 0 && this.blocks[x][y-1] == undefined) this.makeBlock(ROCK,x,y-1);
+				} else {
+					if(y != size - 1 && this.blocks[x][y+1] == undefined) this.makeBlock(ROCK,x,y+1);
+					else if(y != 0 && this.blocks[x][y-1] == undefined) this.makeBlock(ROCK,x,y-1);
+					else if(x != size - 1 && this.blocks[x+1][y] == undefined) this.makeBlock(ROCK,x+1,y);
+					else if(x != 0 && this.blocks[x-1][y] == undefined) this.makeBlock(ROCK,x-1,y);
+					
+				}
+			} else
+				-- i;
+		}
+	}
 }
+
