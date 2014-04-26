9e0b43bc543b3c3a3e24385af908f597a8eff09b
Anthony Foster <git@af.id.au>
2014-04-26 15:25:26 +1000

    keyboard module

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var scene = new THREE.Scene();
 window.scene = scene;
 var camera = new THREE.OrthographicCamera(-0.5, 9.5, -0.5, 9.5, 0, 10);
 
+var Keyboard = require('./objects/keyboard');
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -31,7 +33,21 @@ var render = function () {
   renderer.render(scene, camera);
 };
 
+var keys = new Keyboard();
 
+keys.onleft = function () {
+  console.log('LEFT!!!');
+};
+keys.onright = function () {
+  console.log('RIGHT!!!');
+
+};
+keys.onup = function () {
+  console.log('UP!!!');
+};
+keys.ondown = function () {
+  console.log('DOWN!!!');
+};
 
 window.onload = function(){
 	// cube = new Digulator.Sand();
---
+++ objects/keyboard.js
@@ -0,0 +1,46 @@
+module.exports = KeyboardState;
+
+function keydown(event) {
+  if (event.keyCode === KeyboardState.ALIAS['left']) {
+    return this.onleft();
+  } else if (event.keyCode === KeyboardState.ALIAS['right']) {
+    return this.onright();
+  } else if (event.keyCode === KeyboardState.ALIAS['up']) {
+    return this.onup();
+  } else if (event.keyCode === KeyboardState.ALIAS['down']) {
+    return this.ondown();
+  }
+}
+
+function keyup(event) {
+
+}
+
+function KeyboardState() {
+  this.keyCodes = {};
+  this.modifiers  = {};
+
+  var self  = this;
+  this._onKeyDown = keydown.bind(this);
+  this._onKeyUp = keyup.bind(this);
+
+  document.addEventListener('keydown', this._onKeyDown, false);
+  document.addEventListener('keyup', this._onKeyUp, false);
+}
+
+KeyboardState.prototype.destroy  = function() {
+  document.removeEventListener('keydown', this._onKeyDown, false);
+  document.removeEventListener('keyup', this._onKeyUp, false);
+};
+
+KeyboardState.MODIFIERS  = ['shift', 'ctrl', 'alt', 'meta'];
+KeyboardState.ALIAS  = {
+  'left'    : 37,
+  'up'      : 38,
+  'right'   : 39,
+  'down'    : 40,
+  'space'   : 32,
+  'pageup'  : 33,
+  'pagedown': 34,
+  'tab'     : 9
+};
