8c5ae17b2722533e61dd246d2deecebc0691e1da
Clark <calrk1@gmail.com>
2014-04-27 12:23:39 +1000

    Sounds

--- objects/soundPlayer.js
+++ objects/soundPlayer.js
@@ -5,8 +5,32 @@ function SoundPlayer() {
   this.audio = new AudioContext();
   this.sounds = {};
 
+  //Drill
   this.loadSound('DrillFast');
   this.loadSound('DrillMed');
+  this.loadSound('Laser');
+  this.loadSound('Laser2');
+
+  //atmospheric
+  this.loadSound('Bird1');
+  this.loadSound('Bird2');
+  this.loadSound('Bird3');
+  this.loadSound('Wind1');
+  this.loadSound('Wind2');
+
+  //misc
+  this.loadSound('DestroyTree');
+  this.loadSound('Engine');
+  this.loadSound('Explosion');
+  this.loadSound('Grenade');
+  this.loadSound('Rocket');
+  this.loadSound('Sand');
+
+  //Success
+  this.loadSound('Gold');
+
+  //Death
+  this.loadSound('Death');
 }
 
 SoundPlayer.prototype.play = function(sound) {
